263cc4f70ff1bd95ad08d0ed65936eee47cea998
Little Chestnut <6192946+2012013382@users.noreply.github.com>
2018-05-24 15:41:27 +0800

    Update test.py

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ import tensorflow.contrib.slim as slim
 import tensorflow.contrib.slim.nets as nets
 import csv as csv
 
-CHECK_POINT_PATH = 'check_point/train_model.ckpt-9'
+CHECK_POINT_PATH = 'check_point/train_model.ckpt-2'
 NUM_KAGGLE_TEST = 12500
 BATCH_SIZE = 50
 batch_test_set = data_processing.pre_processing(data_set='test', batch_size=BATCH_SIZE)
@@ -18,14 +18,14 @@ prediction_file_object = csv.writer(prediction_file)
 prediction_file_object.writerow(['id', 'label'])
 
 with tf.Graph().as_default():
-    test_images = tf.placeholder(tf.float32, [BATCH_SIZE, 224, 224, 3])
-    test_labels = tf.placeholder(tf.float32, [BATCH_SIZE, len(data_processing.IMG_CLASSES)])
-    logits, _ = nets.vgg.vgg_16(inputs=test_images, num_classes=2, is_training=False)
+    images = tf.placeholder(tf.float32, [BATCH_SIZE, 224, 224, 3])
+    keep_prob = tf.placeholder(tf.float32)
+    logits, _ = nets.vgg.vgg_19(inputs=images, num_classes=2, dropout_keep_prob=keep_prob, is_training=False)
     variables_to_restore = slim.get_variables_to_restore()
     restorer = tf.train.Saver(variables_to_restore)
 
     pros = tf.nn.softmax(logits)
-
+    
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     with tf.Session(config=config) as sess:
@@ -34,8 +34,13 @@ with tf.Graph().as_default():
         restorer.restore(sess, CHECK_POINT_PATH)
 
         for i in range(NUM_KAGGLE_TEST // BATCH_SIZE):
-            batch_pros = sess.run(pros, feed_dict={test_images: batch_test_set['images'][i]})
+            batch_pros = sess.run(pros, feed_dict={images: batch_test_set['images'][i], keep_prob: 1.0})
             print('Batch %d of %d'%(i, NUM_KAGGLE_TEST // BATCH_SIZE))
-            for j in range(BATCH_SIZE):                    
-                prediction_file_object.writerow([ i * BATCH_SIZE + j + 1, batch_pros[j, 1]])
+            for j in range(BATCH_SIZE):
+                temp = 0.0
+                if batch_pros[j, 1] > 0.5:
+                   temp = 0.995
+                else:
+                   temp = 0.005                 
+                prediction_file_object.writerow([ i * BATCH_SIZE + j + 1, temp])
 prediction_file.close()
